9b89724d1dc3f3b3dc980a4443d92b280652b0e4
Hakim ghani <57526200+dilkax1023@users.noreply.github.com>
2020-09-17 12:07:04 +0200

    debug

--- models/product.js
+++ models/product.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
@@ -16,28 +15,3 @@ const productSchema = new mongoose.Schema({
 const Product = mongoose.model('Product', productSchema);
 
 module.exports = Product;
-=======
-const mongoose = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose'); 
-
-const ProductsSechema = new mongoose.Schema({
-    user:{ 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'user' 
-    },
-    name: String, 
-    price: String,
-    description: String,
-    created: {
-        type: Date,
-        default: Date.now
-    }
-}); 
-
-ProductsSechema.plugin(passportLocalMongoose);
-
-const Products = mongosse.model('Products', ProductsSchema);
-
-module.exports = Products;
-
->>>>>>> 29671a463656bf024d1409e0e4f0bd7e5ffe929e
